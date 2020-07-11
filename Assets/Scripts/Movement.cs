d874858c3de9bd548260540d97a3ed778ea29bb3
Charles <chaoliner@gmail.com>
2020-07-10 21:57:36 -0400

    Tweaked Movement.cs to work more smoothly

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,62 +3,38 @@ using System.Collections;
  
 public class Movement : MonoBehaviour {
  
-    public float playerSpeed;
-    public float sprintSpeed = 4f;
-    public float walkSpeed = 2f;
-    public float mouseSensitivity = 2f;
-    public float jumpHeight = 3f;
-    private bool isMoving = false;
-    private bool isSprinting =false;
-    private float yRot;
+    //these can be constants
+    const float sprintSpeed = 15f;
+    const float walkSpeed = 7.5f;
+    const float acceleration = 60f;
+    const float jumpSpeed = 15f;
+
+    public new Rigidbody rigidbody;
  
-    private Rigidbody rigidBody;
- 
-    // Use this for initialization
     void Start () {
- 
-        playerSpeed = walkSpeed;
-        rigidBody = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
- 
     }
- 
-    // Update is called once per frame
+
+    private void Update() {
+        //GetKeyDown/GetButtonDown will sometimes drop inputs when called from FixedUpdate (which can sometimes be called more often than the input system updates)
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+        }
+    }
+
     void FixedUpdate () {
- 
-        yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, yRot, transform.localEulerAngles.z);
- 
-        isMoving = false;
- 
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-        {
-            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * 0.1f);
-            //GetComponent<Rigidbody>().velocity += transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed * 0.1f;
-            isMoving = true;
-        }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-        {
-            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * playerSpeed * 0.1f);
-            //GetComponent<Rigidbody>().velocity += transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed * 0.1f;
-            isMoving = true;
-        }
-        
- 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight * 10f);
-        }
- 
-        if (Input.GetAxisRaw("Sprint") > 0f)
-        {
-            playerSpeed = sprintSpeed;
-            isSprinting = true;
-        }else if (Input.GetAxisRaw("Sprint") < 1f)
-        {
-            playerSpeed = walkSpeed;
-            isSprinting = false;
-        }
- 
+
+        //isSprinting and playerSpeed can be declared locally
+        bool isSprinting = Input.GetButton("Sprint");
+        float playerSpeed = isSprinting ? sprintSpeed : walkSpeed;
+
+        Vector3 horizontalVelocity = rigidbody.velocity;
+        horizontalVelocity.y = 0;
+
+        Vector3 targetHorizontalVelocity = transform.right * Input.GetAxisRaw("Horizontal") * playerSpeed +
+            transform.forward * Input.GetAxisRaw("Vertical") * playerSpeed;
+
+        //rigidbodies should not be moved with transform.Translate
+        rigidbody.AddForce(Vector3.ClampMagnitude(targetHorizontalVelocity - horizontalVelocity, Time.deltaTime * acceleration), ForceMode.VelocityChange);
     }
 }
