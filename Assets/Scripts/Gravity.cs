37d6608e20387023bd7fdbf1bb1ddc971213e44f
Cheeseboy100 <jbb2727@gmail.com>
2020-07-11 12:35:30 +0100

    Add files via upload
    Script for the change in gravity, although i can't figure out how to get moving objects to change their trajectory. It is hard to explain, test and see.
    
    Conductor script keeps track of position in song and where beats are in a song are.

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,61 @@
+﻿// Cannot change direction once gravity has been changed right now
+using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour
+{
+    Vector3 gravity;
+    public bool GravityXMinus = false;
+    public bool GravityYMinus = false;
+    public bool GravityZMinus = false;
+    public bool GravityXPlus = false;
+    public bool GravityYPlus = false;
+    public bool GravityZPlus = false;
+ 
+ void Start()
+ {
+     gravity = Physics.gravity;
+ }
+
+ void FixedUpdate()
+ {
+     Physics.gravity = gravity;
+     
+     if(GravityZMinus)
+     {
+         gravity.x = 0;
+         gravity.y = 0;
+         gravity.z = -25;
+     }
+     if(GravityXMinus)
+     {
+         gravity.x = -25;
+         gravity.y = 0;
+         gravity.z = 0;
+     }
+      if(GravityYMinus)
+     {
+         gravity.x = 0;
+         gravity.y = -25;
+         gravity.z = 0;
+     }
+     if(GravityZPlus)
+     {
+         gravity.x = 0;
+         gravity.y = 0;
+         gravity.z = 25;
+     }
+     if(GravityXPlus)
+     {
+         gravity.x = 25;
+         gravity.y = 0;
+         gravity.z = 0;
+     }
+      if(GravityYPlus)
+     {
+         gravity.x = 0;
+         gravity.y = 25;
+         gravity.z = 0;
+     }
+ }    
+}
