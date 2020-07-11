ad8f23c4fca53e2e77743541a0b4950a040a388c
Charles <chaoliner@gmail.com>
2020-07-10 22:07:09 -0400

    Added Gun.cs back, don't know how it was deleted Changed "Sprint" (which doesn't exist) to "Fire3" in Movement.cs

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public new Rigidbody rigidbody;
+    public Transform shootHole;
+    public ParticleSystem muzzleFlash;
+
+    const float shootForce = 5;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            rigidbody.AddForceAtPosition(-shootHole.forward * shootForce, shootHole.position, ForceMode.VelocityChange);
+            muzzleFlash.Play();
+        }
+    }
+}
